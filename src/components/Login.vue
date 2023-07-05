a736b26f504d29c7062f3c866adb509ee550d1d0
zhuolin <zhuolin1014@163.com>
2023-07-05 20:07:40 +0800

    注册登录添加过度效果

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,20 +3,46 @@
     <div class="modal-mask">
       <div class="modal-wrapper">
         <div class="modal-container">
-          <LottieSvg :options='defaultOptions' class="main"/>
+          <LottieSvg :options="defaultOptions" class="main" />
           <div class="form">
-            <h3>创建账户</h3>
-            <div v-show="true" class="register">
-              <input type="text" placeholder="用户名" />
-              <input type="password" placeholder="密码" />
-              <div class="button">创建账号</div>
+            <h3 @click="selectedShow">创建账户</h3>
+
+            <div :class="{show: isShowRegister}" class="register">
+              <input
+                type="text"
+                v-model="register.username"
+                placeholder="用户名"
+              />
+              <input
+                type="password"
+                v-model="register.password"
+                @keyup.enter="onRegister"
+                placeholder="密码"
+              />
+              <p v-bind:class="{ error: register.isError }">
+                {{ register.notice }}
+              </p>
+              <div class="button" @click="onRegister">创建账号</div>
             </div>
-            <h3>登录</h3>
-            <div v-show="false" class="login">
-              <input type="text" placeholder="输入用户名" />
-              <input type="password" placeholder="密码" />
-              <div class="button">登录</div>
+
+            <h3 @click="selectedShow">登录</h3>
+
+            <div :class="{show: isShowLogin}" class="login">
+              <input
+                type="text"
+                v-model="login.username"
+                placeholder="输入用户名"
+              />
+              <input
+                type="password"
+                v-model="login.password"
+                @keyup.enter="onLogin" 
+                placeholder="密码"
+              />
+              <p v-bind:class="{ error: login.isError }">{{ login.notice }}</p>
+              <div class="button" @click="onLogin">登录</div>
             </div>
+
           </div>
         </div>
       </div>
@@ -26,7 +52,7 @@
 
 <script>
 import LottieSvg from "@/components/LottieSvg";
-import animationData from '@/assets/svg/notebook'
+import animationData from "@/assets/svg/notebook";
 export default {
   components: {
     LottieSvg
@@ -37,8 +63,67 @@ export default {
         animationData: animationData,
         autoplay: true,
         loop: true
+      },
+      isShowLogin: true,
+      isShowRegister: false,
+      login: {
+        username: "",
+        password: "",
+        notice: "输入用户名和密码",
+        isError: false
+      },
+      register: {
+        username: "",
+        password: "",
+        notice: "创建账号后，请记住用户名和密码",
+        isError: false
       }
     };
+  },
+  methods: {
+    selectedShow() {
+      this.isShowLogin = !this.isShowLogin;
+      this.isShowRegister = !this.isShowRegister;
+    },
+    // showRegister() {
+    //   this.isShowLogin = false;
+    //   this.isShowRegister = true;
+    // },
+    onRegister() {
+      if (!/^[\w\u4e00-\u9fa5]{3,15}$/.test(this.register.username)) {
+        this.register.isError = true;
+        this.register.notice = "用户名3~15个字符，仅限于字母数字下划线中文";
+        return;
+      }
+      if (!/^.{6,16}$/.test(this.register.password)) {
+        this.register.isError = true;
+        this.register.notice = "密码长度为6~16个字符";
+        return;
+      }
+      this.register.isError = false;
+      this.register.notice = "";
+      console.log(
+        `start register..., username: ${this.register.username} , password: ${this.register.password}`
+      );
+    },
+    onLogin() {
+      if (!/^[\w\u4e00-\u9fa5]{3,15}$/.test(this.login.username)) {
+        this.login.isError = true;
+        this.login.notice = "用户名3~15个字符，仅限于字母数字下划线中文";
+        return;
+      }
+      if (!/^.{6,16}$/.test(this.login.password)) {
+        this.login.isError = true;
+        this.login.notice = "密码长度为6~16个字符";
+        return;
+      }
+      this.login.isError = false;
+      this.login.notice = "";
+
+      console.log(
+        `start login..., username: ${this.login.username} , password: ${this.login.password}`
+      );
+    }
   }
 };
 </script>
@@ -106,8 +191,15 @@ export default {
 
     .login,
     .register {
-      padding: 10px 20px;
+      padding: 0px 20px;
       border-top: 1px solid #eee;
+      height: 0;
+      overflow: hidden;
+      transition: height 0.4s;
+
+      &.show {
+        height: 193px;
+      }
 
       input {
         display: block;
